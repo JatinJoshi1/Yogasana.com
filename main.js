7dbab9708e780f34ca2aca5574f91c7effffbcfc
Yogasana <91771341+JatinJoshi1@users.noreply.github.com>
2023-10-08 06:38:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -103,3 +103,47 @@ window.addEventListener('load', () => {
     preloader.style.zIndex = '-1';
   }, 3000); // Add a delay of 2000ms (2 seconds) before hiding the preloader
 });
+let cards = document.querySelectorAll(".card");
+     let stackArea = document.querySelector(".stack-area");
+
+     function rotateCards() {
+       let angle = 0;
+       cards.forEach((card) => {
+         if (card.classList.contains("active")) {
+           card.style.transform = `translate(-50%, -120vh) rotate(-48deg)`;
+         } else {
+           card.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+           angle = angle - 10;
+         }
+       });
+     }
+
+     rotateCards();
+
+     window.addEventListener("scroll", () => {
+       let proportion =
+         stackArea.getBoundingClientRect().top / window.innerHeight;
+       if (proportion <= 0) {
+         let n = cards.length;
+         let index = Math.ceil((proportion * n) / 2);
+         index = Math.abs(index) - 1;
+         for (let i = 0; i < n; i++) {
+           if (i <= index) {
+             cards[i].classList.add("active");
+           } else {
+             cards[i].classList.remove("active");
+           }
+         }
+         rotateCards();
+       }
+     });
+     function updateProgressBar() {
+                 const windowHeight = window.innerHeight;
+                 const documentHeight = document.body.clientHeight;
+                 const scrollTop = window.scrollY;
+                 const progress = (scrollTop / (documentHeight - windowHeight)) * 100;
+                 document.getElementById('progress-bar').style.width = `${progress}%`;
+             }
+
+             // Add an event listener to update the progress bar on scroll
+             window.addEventListener('scroll', updateProgressBar);
